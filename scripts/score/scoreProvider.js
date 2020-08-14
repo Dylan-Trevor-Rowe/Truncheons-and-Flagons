860a6fc55e5836a3db75ca3cc87422d520446061
Raparker88 <raparker88@gmail.com>
2020-08-14 10:47:36 -0500

    updated scoreprovider.js

--- scripts/score/scoreProvider.js
+++ scripts/score/scoreProvider.js
@@ -1,33 +1,14 @@
+import { getTeams, useTeams } from "../team/teamProvider.js"
+
 const eventHub = document.querySelector(".container")
 
-let scores = []
-
-const dispatchChangeEvent = () => {
-    const scoreStateChangedEvent = new CustomEvent("scoreStateChanged")
-    eventHub.dispatchEvent(scoreStateChangedEvent)
-}
-
 export const getScores = () => {
-    return fetch('http://localhost:3000/teamScores')
-        .then(response => response.json())
-        .then(parsedScores => {
-            scores = parsedScores
+    getTeams()
+    .then(() => {
+        const teams = useTeams()
+        const scores = teams.map(team => {
+            return {totalScore: team.totalScore, teamName: team.name}
         })
-
-}
-
-export const saveScore = score => {
-    return fetch('http://localhost:3000/teamScores', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(score)
+        return scores
     })
-    .then(getScores)
-    .then(dispatchChangeEvent)
-}
-
-export const useScores = () => {
-    return scores.slice()
 }
