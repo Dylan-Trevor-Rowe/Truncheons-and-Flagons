4f2badce71675c9ea3f842c028c8de13871d6e91
FORDBA <bfordcpa@gmail.com>
2020-08-14 15:25:06 -0500

    added listener for game setup

---
+++ scripts/landing page/setupGamePage.js
@@ -0,0 +1,10 @@
+import { TeamForm } from "../Forms/teamForm.js";
+const eventHub=document.querySelector(".container")
+
+
+eventHub.addEventListener("landingPageStart", () => {
+   
+           TeamForm()
+       
+
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 
 
-
+import "./landing page/setupGamePage.js"
 import { leaveThePage, landingButton } from './landing page/landingPage.js'
 
  landingButton();
  leaveThePage();
+ 
