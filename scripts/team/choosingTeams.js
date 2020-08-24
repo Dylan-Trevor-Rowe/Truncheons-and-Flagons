48946b716483eb0e4802eabacd6c300039729bca
Mark Skinner <jmskinne@gmail.com>
2020-08-24 14:49:26 -0500

    sorted by totalScore

--- scripts/team/choosingTeams.js
+++ scripts/team/choosingTeams.js
@@ -25,11 +25,15 @@ export const teamList = () => {
 }
 
 export const teamsPlayingSelects = () => {
-    contentTargetTwo.innerHTML = `
+    
+    contentTargetTwo.innerHTML = 
+    
+    `
 <select id="unoSelect" class="selectOne">
 <option value="0"></option>
 ${
       teams.map(team => {
+          
             return `<option value="${team.id}"> ${team.name}</option>`;
         }).join('')
         }    
